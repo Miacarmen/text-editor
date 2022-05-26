ae3e6a49fd1f2e360466efc8b2d76d28f9d5f962
miacarmen <maitreyacarmen@gmail.com>
2022-05-25 17:18:10 -0700

    configured webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,77 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+// require InjectManifest class of WorkBoxPlugin
+const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// Configure workbox plugins for a service worker and manifest file.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
+    // entry point for files
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
+    // outputs dist for bundles
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      // webpack that generates html file and injects bundles
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "JATE",
+      }),
+      // injects custom service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      // creates manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Take notes with Javascript syntax highlighting!",
+        background_color: "#21211e",
+        theme_color: "#352442",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            size: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
-
+    // Add CSS loaders and babel to webpack.
     module: {
+      // CSS loaders
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+          // use: [MiniCssExtractPlugin.loader, 'css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // use babel-loader in order to use ES6
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
+// require express
 const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// serves static files in client dist folder
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// require HTML routes
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
