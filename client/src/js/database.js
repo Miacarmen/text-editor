e5535572204c68cc009f06ac9662e11e7cdbb5da
miacarmen <maitreyacarmen@gmail.com>
2022-05-26 22:02:05 -0700

    unsuccessful attempt at fixing manifest error

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,16 +1,18 @@
 import { openDB } from "idb";
 
+const jateDB = 'jate';
+
 const initdb = async () =>
   // creating new DB called 'jate', using version 1
-  openDB("jate", 1, {
+  openDB(jateDB, 1, {
     // adding database schema if it doesn't already exist
     upgrade(db) {
-      if (db.objectStoreNames.contains("jate")) {
+      if (db.objectStoreNames.contains(jateDB)) {
         console.log("jate database already exists");
         return;
       }
       // create new object store for the data with a key name of 'id'
-      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      db.createObjectStore(jateDB, { keyPath: "id", autoIncrement: true });
       console.log("jate database created");
     },
   });
@@ -18,10 +20,10 @@ const initdb = async () =>
 // ADD content to the database
 export const putDb = async (content) => {
   console.log("PUT request to database");
-  const jateDb = await openDB("jate", 1);
-  const tx = jateDb.transaction("jate", "readwrite");
-  const store = tx.objectStore("jate");
-  const request = store.put({ jate: content });
+  const jateDb = await openDB(jateDB, 1);
+  const tx = jateDb.transaction(jateDB, "readwrite");
+  const store = tx.objectStore(jateDB);
+  const request = store.put({ id: 1, content: content });
   const result = await request;
   console.log("🚀 - data saved to the database", result);
 };
@@ -30,17 +32,17 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   console.log("GET request to database");
   // create connection to the database and version we want to use
-  const jateDb = await openDB("jate", 1);
+  const jateDb = await openDB(jateDB, 1);
   // new transaction with specified database and data privileges
-  const tx = jateDb.transaction("jate", "readonly");
+  const tx = jateDb.transaction(jateDB, "readonly");
   // open desiered object store
-  const store = tx.objectStore("jate");
+  const store = tx.objectStore(jateDB);
   // use getAll method to get all data in the database
   const request = store.getAll();
   // get confirmation of the request
   const result = await request;
   console.log("result.value", result);
-  // return result;
+  return result.content;
 };
 
 initdb();
